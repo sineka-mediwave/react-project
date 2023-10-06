e68839c3fb3f72bbdc6c4be4b6f10f5d6a7b4e23
sineka-mediwave <sineka@mindwaveventures.com>
2023-10-06 11:43:58 +0530

    without-formik

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as Yup from "yup";
+import { useState } from "react";
 import Input from "./Components/Input";
 
 function App() {
@@ -10,69 +9,38 @@ function App() {
     ratings: 0,
     url: "",
   };
-
-  const validationSchema = Yup.object().shape({
-    title: Yup.string().required("Title is required"),
-    description: Yup.string()
-      .required("Description is required")
-      .min(5, "Description should be at least 5 characters"),
-    ratings: Yup.number()
-      .required("Ratings is required")
-      .max(10, "Rating cannot be greater than 10"),
-    url: Yup.string().url("Enter a valid URL").required("URL is required"),
-  });
+  const [] = useState(initialValues);
   return (
     <div className="App">
       <main className="App-header">
         <h1>Movie Card Form</h1>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={validationSchema}
-          onSubmit={(values, actions) => {
-            // alert(JSON.stringify(values, null, 2));
-            console.log(values);
-            actions.setSubmitting(false);
-          }}
-        >
-          {({ setFieldValue,touched,error }) => (
-            <Form>
-              <Field
-                type="text"
-                name="title"
-                id="title"
-                placeholder="movie name"
-              />
-              <ErrorMessage name="title" component="div" />
+            <form>             
               <Input
                 name="title"
                 type="text"
                 id="title"
                 label="Enter Movie Name"
                 placeholder="movie Name"
-                onChange = {(e) => setFieldValue('title', e.target.value)}
+                // onChange = {(e) => setFieldValue('title', e.target.value)}
                 // onBlur = {()=> (...touched, "title":true)}
               />
 
-              <Field
+              <Input
                 type="number"
                 name="ratings"
                 id="ratings"
                 placeholder="rate the movie"
               />
-              <ErrorMessage name="ratings" component="div" />
 
-              <Field
+              <Input
                 type="text"
                 name="description"
                 placeholder="write about movie"
                 id="description"
               />
-              <ErrorMessage name="description" />
 
               <button type="submit" >Submit</button>
-            </Form>
-          )}
-        </Formik>
+            </form>
       </main>
     </div>
   );
